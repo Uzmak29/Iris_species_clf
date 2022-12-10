b4f70b7b575bd5a6193071fe954eeb9de138bd2c
Uzmak29 <120245844+Uzmak29@users.noreply.github.com>
2022-12-10 14:27:21 +0530

    Add files via upload

---
+++ modules.sh
@@ -0,0 +1,11 @@
+pip3 install numpy==1.19
+
+pip3 install pandas==1.1.5
+
+pip3 install scipy==1.4.1
+
+pip3 install matplotlib==3.2.2
+
+pip3 install seaborn==0.11.1
+
+pip3 install scikit-learn==0.22.2
